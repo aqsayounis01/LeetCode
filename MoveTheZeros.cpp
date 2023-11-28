bba323aede1bdabe5ec2575a5a13aabeca34a10d
Aqsa Younis <aqsayounis2004@gmail.com>
2023-11-28 18:22:14 +0530

    vectors

---
+++ MoveTheZeros.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& v) {
+      
+        int c=0;
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]==0)
+            {
+                v.erase(v.begin()+i);
+                i--;
+               c++;
+
+            }
+        }
+        for(int i=0;i<c;i++)
+        {
+            v.push_back(0);
+        }
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int>v;
+    int n;
+    cin>>n;
+    for (int  i = 0; i < n; i++)
+    {
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    
+    s.moveZeroes()
+
+}
